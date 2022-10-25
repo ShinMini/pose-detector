80f73fc229364169f9f10185d0108f7123b4b139
Shin__Mini <gusals121234@gmail.com>
2022-10-25 21:44:40 +0900

    end

--- src/components/Balance/ButtonSection.tsx
+++ src/components/Balance/ButtonSection.tsx
@@ -5,18 +5,27 @@ import styled from 'styled-components/native'
 import RegularButton from '../Buttons/RegularButton'
 import { Ionicons } from '@expo/vector-icons'
 import { colors } from '../colors'
-import MediaPicker from '../Picker/MediaPicker'
 
 const ButtonSectionBackground = styled.View`
   width: 90%;
   align-items: center;
   height: 20%;
 `
+// navigation
+import { useNavigation } from '@react-navigation/native'
+import type { Props as HomeProps } from '../../screens/Home'
 
-const ButtonSection: FC = () => {
+const ButtonSection: FC = (props) => {
+  // configuring navigation
+  const navigation = useNavigation<HomeProps['navigation']>()
+
+  // move to balance page
+  const handlePress = () => {
+    navigation.navigate('MediaPicker')
+  }
   return (
     <ButtonSectionBackground>
-      <RegularButton btnStyles={{}} onPress={(notUsed) => <MediaPicker />}>
+      <RegularButton btnStyles={{}} onPress={handlePress}>
         내 동작 확인하기
         <Ionicons size={17} name="card" color={colors.white} />
       </RegularButton>
